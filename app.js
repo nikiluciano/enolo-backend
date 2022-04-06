2663cef577a641943b1d48251e61f3a4491fbe05
alessandro_cancelliere <alessandro@cancelliere.info>
2022-04-06 12:11:04 +0200

    package.json updated and .env file added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
 const app = express();
+require("dotenv/config");
+
+const port = process.env.PORT
 
 app.get("/", (req, res) => {
     res.send("Welcome to enolo's backend");
 })
 
-app.listen("4000", function () {
-    console.log("Server is running on port 4000");
+app.listen(port, function () {
+    console.log("Server is running on port: " + port);
 });
