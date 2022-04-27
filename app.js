6d14d426729ab54c7d88b4c8d35748fc82e34f13
alessandro_cancelliere <alessandro@cancelliere.info>
2022-04-27 14:19:34 +0200

    feat: db connection

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
-
+const mongoose = require("mongoose")
 const app = express();
+require("dotenv/config");
+
+const port = process.env.PORT
+const dbUrl = process.env.DB_URL
 
 app.get("/", (req, res) => {
     res.send("Welcome to enolo's backend");
 })
 
-app.listen("4000", function () {
-    console.log("Server is running on port 4000");
+//Connect to DB
+mongoose.connect(
+    dbUrl,
+    { useNewUrlParser: true },
+    () => console.log("Connected to DB!")
+);
+
+//Create server
+app.listen(port, function () {
+    console.log("Server is running on port: " + port);
 });
