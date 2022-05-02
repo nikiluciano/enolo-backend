e8ed026d4d641785d887a79892bdaf3e3ad7e2b1
alessandro_cancelliere <alessandro@cancelliere.info>
2022-05-03 00:33:16 +0200

    fix: spaces in userReq fields

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -2,23 +2,31 @@ const bcrypt = require("bcrypt");
 const userModel = require("../models/user");
 
 module.exports = async function registerUser(req,res) {
-    const password = req.body.password;
+    //fun replace removes all white spaces
+    const passwordReq = req.body.password;
+    const usernameReq = req.body.username.replace(/ /g, '');
+    const emailReq = req.body.email.replace(/ /g, '');
+    const nameReq = req.body.name.replace(/ /g, '');
+    const surnameReq = req.body.surname.replace(/ /g, '');
+    const phoneReq = req.body.phone.replace(/ /g, '');
+    const addressReq = req.body.address.replace(/ /g, '');
+    const roleReq = "WORKER"
 
-    if(password === ""){
+    if(passwordReq === ""){
         res.status(404).json( {msg:"Password field should not be empty"} );
         return
     }
 
-    const encryptedPassword = await bcrypt.hash(password,10);
+    const encryptedPassword = await bcrypt.hash(passwordReq,10);
     const newUser = new userModel({
-        username: req.body.username,
+        username: usernameReq,
         password: encryptedPassword,
-        email: req.body.email,
-        name: req.body.name,
-        surname: req.body.surname,
-        phone: req.body.phone,
-        address: req.body.address,
-        role: "WORKER"
+        email: emailReq,
+        name: nameReq,
+        surname: surnameReq,
+        phone: phoneReq,
+        address: addressReq,
+        role: roleReq
     });
 
     const found = await userModel.findOne({username:req.body.username}).exec();
